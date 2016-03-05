e5c7895d5d8076817cc11c5b278317dbe0203528
gabanida <gabanida@mail.uc.edu>
2016-03-04 22:29:29 -0500

    Initiated tweetDeletion file; added .txt file for access to dummy account

---
+++ tweetDeletion.py
@@ -0,0 +1,9 @@
+# encoding utf-8
+
+"""
+Author:         David Gabanic
+Description:    This program will delete all tweets after a specific number of days
+                in order to avoid future embarassing moments courtesy of Timehop.
+"""
+
+import tweepy
