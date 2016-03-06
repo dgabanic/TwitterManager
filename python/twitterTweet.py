0884be3463137f39a115dd6f34c3ff669f47757c
gabanida <gabanida@mail.uc.edu>
2016-03-05 19:36:35 -0500

    Added, refactored, figured out program automation
    - Added send_scheduled_tweet function which automatically tweets the
    current time
    - Added send_tweet function increase readability
    - Figured out how to automatically run python at a scheduled time, the
    XML file is a byproduct of that

--- python/twitterTweet.py
+++ python/twitterTweet.py
@@ -8,11 +8,24 @@ Description:    This script allows the user to tweet from the command line.
 
 import tweepy
 import sys
+import time
+import threading
 
 import twitterLogin
+
+# Tweets the time every minute
+def send_scheduled_tweet():
+    api.update_status(time.ctime())
+    print("Tweet sent by @%s" % api.me().screen_name)
+    threading.Timer(60, send_tweet).start()
+
+# Tweets user input from command line
+def send_tweet(tweet):
+    api.update_status(tweet)
+    print("Tweet sent by @%s" % api.me().screen_name)
 
 if __name__ == "__main__":
     api = twitterLogin.oauth_login(twitterLogin.CONSUMER_KEY, twitterLogin.CONSUMER_SECRET)
     print ("Authenticated as @%s" % api.me().screen_name)
 
-    api.update_status(sys.argv[1])
+    send_tweet(sys.argv[1])
